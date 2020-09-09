e0f1336a08fc4ae499e61bb66b933f47ec897a27
Rebecca Robbins <rebeccamargaret13@gmail.com>
2020-09-09 18:11:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,12 +3,10 @@ katz_deli = [ ]
 def line(line)
   if line == [ ]
     puts "The line is currently empty."
-  else
-    sentence = "The line is currently:"
+  if line != [ ]
     line.each.with_index do |name, index|
-      sentence += " #{index + 1}. #{name}"
+      puts "The line is currently: #{index+1}. #{name}"
     end
-    puts sentence
   end
 end
 
